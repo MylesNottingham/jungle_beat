32a38b9f9b18d5605a506d17b442fe63439d71de
Myles Nottingham <MylesNottingham@gmail.com>
2023-03-24 15:37:12 -0600

    rewrite count and append methods in linked_list.rb

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,13 +1,37 @@
 class LinkedList
-  attr_reader :head
+  attr_reader :head, :to_string
   
   def initialize
     @head = nil
-    @count = nil
-    @to_string = nil
+    @to_string = @head.data if @head
+  end
+
+  def count
+    if @head
+      count = 1
+      last_node = @head.next_node
+      while last_node
+        count += 1
+        last_node = last_node.next_node
+      end
+    else
+      count = 0
+    end
+    count
   end
 
   def append(node)
-    @head = Node.new(node)
+    if @head
+      next_node = @head
+      last_node = @head.next_node
+      until last_node == nil
+        next_node = last_node
+        last_node = last_node.next_node
+      end
+      next_node.set_next_node(Node.new(node))
+    else
+      @head = Node.new(node)
+    end
+    node
   end
 end
