7df5a43284308fce893bf457ec07ea84c1f23c34
Myles Nottingham <MylesNottingham@gmail.com>
2023-03-24 12:59:52 -0600

    Break second test into two in node_spec.rb

--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -10,8 +10,11 @@ RSpec.describe Node do
     expect(@node).to be_an_instance_of(Node)
   end
 
-  it "has readable attributes" do
+  it "has can hold data" do
     expect(@node.data).to eq("plop")
+  end
+
+  it "begins without a next node" do
     expect(@node.next_node).to eq(nil)
   end
 end
