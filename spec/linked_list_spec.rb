f9e3d13eed7067645ee4bd398d16122097610271
Myles Nottingham <MylesNottingham@gmail.com>
2023-03-24 15:54:21 -0600

    Modify tests to support multiple nodes

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -24,10 +24,19 @@ RSpec.describe LinkedList do
     expect(@list.head).to be_a(Node)
   end
 
-  it "can count the number of nodes in the linked list from 0 - 1" do
+  it "can accurately check the next node in the list" do
+    @list.append("doop")
+    expect(@list.head.next_node).to eq(nil)
+    @list.append("deep")
+    expect(@list.head.next_node).to be_a(Node)
+  end
+
+  it "can count the number of nodes in the linked list" do
     expect(@list.count).to eq(0)
     @list.append("doop")
     expect(@list.count).to eq(1)
+    @list.append("deep")
+    expect(@list.count).to eq(2)
   end
 
   it "can read the data of the head node" do
