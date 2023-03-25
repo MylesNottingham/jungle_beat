e70312f04395440ec023dfef8978de899e875fae
Myles Nottingham <MylesNottingham@gmail.com>
2023-03-24 18:56:18 -0600

    Add the insert method to linked_list.rb

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -46,6 +46,32 @@ class LinkedList
     node
   end
 
+  def insert(index, node)
+    length = self.count
+    if (0..length).include? index
+      if index == 0
+        self.prepend(node)
+      elsif @head.next_node == nil
+        self.append(node)
+      else
+        count = 1
+        previous_node = @head
+        slice_node = Node.new(node)
+        next_node = @head.next_node
+        until count == index
+          previous_node = previous_node.next_node
+          next_node = previous_node.next_node
+          count += 1
+        end
+        previous_node.set_next_node(slice_node)
+        slice_node.set_next_node(next_node)
+      end
+    else
+      raise "Invalid Index"
+    end
+    node
+  end
+
   def to_string
     if @head
       all_data = [@head.data]
